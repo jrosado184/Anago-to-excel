66535f6f568451e50ceb84f7021f0bbbd9f84599
Javier Rosado <javierrrosado184@gmail.com>
2024-03-11 22:50:44 -0400

    refactor: refactored convert text to exel code into functions and added error boundaries

--- convert.py
+++ convert.py
@@ -1,8 +1,38 @@
+import os
 import pandas
 
-data = pandas.read_csv(r'C:\Users\javie\OneDrive\Desktop\convert.txt', delimiter='\s+', header=None)
+TEXT_FILE = r'C:\Users\javie\OneDrive\Desktop\convert.txt'
+EXCEL_FILE = r'C:\Users\javie\OneDrive\Desktop\convert.xlsx'
 
-data.to_excel(r'C:\Users\javie\OneDrive\Desktop\convert.xlsx', index=False)
+def convert_text_to_exel() -> bool:
+    if os.path.exists(TEXT_FILE):
+        try:
+            knife_scores = pandas.read_csv(TEXT_FILE, delimiter='\s+', header=None)
+            knife_scores.to_excel(EXCEL_FILE, index=False)
+            print("Successfully converted file to excel")
+            return True
+        except Exception as e:
+            print("Failed to convert file to excel", {e})
+            return False
+    else:
+        print("Text file not found")
+        return False
+    
+        
+        
+def remove_text_file() -> None:
+    if os.path.exists(EXCEL_FILE):
+        try:
+            os.remove(TEXT_FILE)
+        except Exception as e:
+            print("Failed to remove text file", {e})
+    else:
+        print("Excel file not found")
+        
+        
+def main() -> None:
+    if convert_text_to_exel():
+        remove_text_file()    
 
-if __name__ == '__name__':
- print(data)
+if __name__ == '__main__':
+    main()
